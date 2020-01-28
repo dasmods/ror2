5b890d4039b9d2d1afd22e5396e51ef95f7151d2
Jared <jared@jaredjohnson.dev>
2020-01-27 19:34:41 -0500

    Refactor FuncKeyFairy mod.

---
+++ FuncKeyFairy/FuncKeyFairy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+using UnityEngine;
+
+namespace Dasmods
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.dasmods.funckeyfairy", "FuncKeyFairy", "1.0")]
+    public class FuncKeyFairy : BaseUnityPlugin
+    {
+        private GameObject player;
+        private static void dropPickup(PickupIndex pickupIndex, GameObject obj)
+        {
+            Transform transform = obj.transform;
+            Vector3 position = transform.position;
+            Vector3 velocity = transform.forward * 20f;
+            PickupDropletController.CreatePickupDroplet(pickupIndex, position, velocity);
+        }
+
+        private static void dropRandPickup(System.Collections.Generic.List<PickupIndex> dropList, GameObject obj)
+        {
+            PickupIndex pickupIndex = Util.pickRand<PickupIndex>(dropList);
+            dropPickup(pickupIndex, obj);
+        }
+
+        public void Awake()
+        {
+            this.player = PlayerCharacterMasterController.instances[0].master.GetBodyObject();
+        }
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                dropRandPickup(Run.instance.availableTier1DropList, this.player);
+            }
+            else if (Input.GetKeyDown(KeyCode.F3))
+            {
+                dropRandPickup(Run.instance.availableTier2DropList, this.player);
+            }
+            else if (Input.GetKeyDown(KeyCode.F4))
+            {
+                dropRandPickup(Run.instance.availableTier3DropList, this.player);
+            }
+        }
+    }
+}
---
+++ FuncKeyFairy/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Dasmods
+{
+    class Util
+    {
+        public static T pickRand<T>(List<T> list)
+        {
+            int randNdx = UnityEngine.Random.Range(0, list.Count);
+            return list[randNdx];
+        }
+    }
+}
