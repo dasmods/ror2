599b65942ca90b8a31d0a47d9f7fecbf06c11527
Jared <jared@jaredjohnson.dev>
2020-02-02 16:49:52 -0500

    Make mines heal in radius over time and explode on end.

---
+++ Mods/HealBois/Heal.cs
@@ -0,0 +1,73 @@
+﻿using EntityStates.Engi.Mine;
+using UnityEngine;
+using UnityEngine.Networking;
+using RoR2;
+
+
+namespace HealBois
+{
+    class Heal : BaseMineState
+    {
+        private static float INITIAL_RADIUS = 10f;
+        private static float MAX_AGE = 10f;
+        private static float DECAY_RATE = INITIAL_RADIUS / MAX_AGE;
+        private static GameObject CreateMushroomWard(Vector3 position)
+        {
+            // create mushroom ward visuals
+            GameObject resource = Resources.Load<GameObject>("Prefabs/NetworkedObjects/MushroomWard");
+            GameObject mushroomWard = UnityEngine.Object.Instantiate(resource, position, Quaternion.identity);
+            mushroomWard.GetComponent<TeamFilter>().teamIndex = TeamIndex.Player;
+            NetworkServer.Spawn(mushroomWard);
+
+            // initialize healing ward props
+            HealingWard healingWard = mushroomWard.GetComponent<HealingWard>();
+            healingWard.healFraction = 0.05f;
+            healingWard.healPoints = 0f;
+            healingWard.Networkradius = INITIAL_RADIUS;
+
+            return mushroomWard;
+        }
+
+        private GameObject mushroomWard;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            mushroomWard = CreateMushroomWard(transform.position);
+        }
+        protected override bool shouldStick
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            mushroomWard.transform.position = transform.position;
+
+            float nextRadius = GetNextRadius();
+
+            if (nextRadius <= 0)
+            {
+                outer.SetNextState(new PreDetonate());
+            }
+
+            HealingWard healingWard = mushroomWard.GetComponent<HealingWard>();
+            healingWard.Networkradius = nextRadius;
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            UnityEngine.Object.Destroy(mushroomWard);
+        }
+
+        private float GetNextRadius()
+        {
+            return (-DECAY_RATE * fixedAge) + INITIAL_RADIUS;
+        }
+    }
+}
--- Mods/HealBois/HealBois.cs
+++ Mods/HealBois/HealBois.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using System.Reflection;
 using BepInEx;
-using RoR2;
+using EntityStates.Engi.Mine;
+using RoR2.Projectile;
+using UnityEngine.Networking;
 
 namespace HealBois
 {
@@ -8,8 +10,24 @@ namespace HealBois
     [BepInPlugin("com.dasmods.healbois", "HealBois", "1.0")]
     public class HealBois : BaseUnityPlugin
     {
-        void Awake() {
-            Chat.AddMessage("HealBois!");
+        private static bool IsMineStuck(WaitForStick waitForStick)
+        {
+            ProjectileStickOnImpact projectileStickOnImpact = (ProjectileStickOnImpact)typeof(WaitForStick)
+                .GetProperty("projectileStickOnImpact", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(waitForStick);
+
+            return projectileStickOnImpact && projectileStickOnImpact.stuck;
+        }
+
+        public void Awake()
+        {
+            On.EntityStates.Engi.Mine.WaitForStick.FixedUpdate += (fixedUpdate, waitForStick) =>
+            {
+                if (NetworkServer.active && IsMineStuck(waitForStick))
+                {
+                    waitForStick.outer.SetNextState(new Heal());
+                }
+            };
         }
     }
 }
