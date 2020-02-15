6849c2281482c2fffa3d2cb64f6369dbea18deb4
Jared <jared@jaredjohnson.dev>
2020-02-15 13:42:15 -0500

    Create ThreatOfThunder mod.

---
+++ Mods/ThreatOfThunder/ThreatOfThunder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace ThreatOfThunder
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.dasmods.threatofthunder", "ThreatOfThunder", "1.0")]
+    public class ThreatOfThunder : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Debug.LogError("ThreatOfThunder loaded!");
+        }
+    }
+}
