a115cfb0823c2b0c0e81c0160252ad7b1bc7a458
Jared <jared@jaredjohnson.dev>
2020-02-15 13:01:31 -0500

    Goto single player lobby immediately after title menu screen load.

--- Mods/Quisk/Quisk.cs
+++ Mods/Quisk/Quisk.cs
@@ -1,25 +1,40 @@
 ﻿using BepInEx;
 using RoR2.UI.MainMenu;
+using RoR2.Networking;
+using UnityEngine.SceneManagement;
 
 namespace Quisk
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.dasmods.quickstart", "Quisk", "1.0")]
+    [BepInPlugin("com.dasmods.quickstart", "Quisk", "1.1")]
     public class Quisk : BaseUnityPlugin
     {
         private bool loaded = false;
 
-        private static void GotoTitleMenuScreen()
+        private static void GoToTitleMenuScreen()
         {
             MainMenuController mainMenuController = FindObjectOfType<MainMenuController>();
             mainMenuController.SetDesiredMenuScreen(mainMenuController.titleMenuScreen);
+        }
+
+        private static void GoToSinglePlayerLobby()
+        {
+            GameNetworkManager gameNetworkManager = FindObjectOfType<GameNetworkManager>();
+            GameNetworkManager.HostDescription.HostingParameters hostingParameters = new GameNetworkManager.HostDescription.HostingParameters
+            {
+                listen = false,
+                maxPlayers = 4
+            };
+            gameNetworkManager.desiredHost = new GameNetworkManager.HostDescription(hostingParameters);
+            SceneManager.LoadScene("lobby", LoadSceneMode.Single);
         }
 
         public void Update()
         {
             if (!loaded)
             {
-                GotoTitleMenuScreen();
+                GoToTitleMenuScreen(); // skip initial loading screen
+                GoToSinglePlayerLobby();
                 loaded = true;
             }
         }
